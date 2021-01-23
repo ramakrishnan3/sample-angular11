91c3026863caa80dbad423974d043250f0f82806
Ramakrishnan-Sathyanarayanan <ramakrishnan.sathyanarayanan@freshworks.com>
2021-01-23 20:47:58 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faHome, faEnvelope,faPhoneAlt, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { Chart } from 'angular-highcharts';
+import { Options } from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,112 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  faPhoneAlt = faPhoneAlt;
+  faGlobe = faGlobe;
+  faHome = faHome;
+  faEnvelope = faEnvelope;
+
+  timelineChart: Chart = new Chart({
+    chart: {
+      type: 'timeline',
+      backgroundColor: '#384b60'
+    },
+    xAxis: {
+      visible: false
+    },
+    yAxis: {
+      visible: false
+    },
+    title: {
+      text: ''
+    },
+    series: [{
+      type: undefined,
+      data: [{
+        name: '2017',
+        label: 'Title',
+        description: 'Company Name'
+      }, {
+        name: '2018',
+        label: 'Title',
+        description: 'Company Name'
+      }, {
+        name: '2019',
+        label: 'Title',
+        description: 'Company Name'
+      }, {
+        name: '2020',
+        label: 'Title',
+        description: 'Company Name'
+      }]
+    }]
+  });
+
+  doughnutChart: Chart = new Chart({
+    chart: {
+        type: 'pie',
+        backgroundColor: '#384b60'
+    },
+    title: {
+        text: ''
+    },
+    plotOptions: {
+        pie: {
+            shadow: false,
+        }
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+    series: [{
+        name: 'Skills',
+        data: [{
+          name: 'Python',
+          y: 23.3,
+          color: '#844d93'
+        }, {
+          name: 'Wireframe',
+          y: 20.9,
+          color: '#ffaa85'
+        }, {
+          name: 'Java',
+          y: 23.3,
+          color: '#ef8987'
+        }, {
+          name: 'HTML/CSS',
+          y: 18.6,
+          color: '#b15a92'
+        }],
+        type: undefined,
+    }]
+})
+  barChart: Chart = new Chart({
+    chart: {
+      type: 'column',
+      backgroundColor: '#384b60'
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+      categories: [
+          'Wix.com',
+          'Weebly',
+          'Wordpress',
+          'Heroku'
+      ],
+      crosshair: true
+    },
+    yAxis: {
+      min: 0,
+    },
+    series: [{
+      type: undefined,
+      name: 'Websites',
+      data: [{color: '#ffaa85', y: 10}, 
+      {color: '#ffaa85', y: 40},
+      {color: '#ffaa85', y: 20},
+      {color: '#ffaa85', y: 40}]
+    }]
+  })
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChartModule } from 'angular-highcharts';
 import { AppComponent } from './app.component';
+import * as Highcharts from 'highcharts';
+declare var require: any;
+require('highcharts/highcharts-more')(Highcharts);
+require('highcharts/modules/timeline')(Highcharts);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
